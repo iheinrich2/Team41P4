96176e6cf4cb71c9fe063ec758701a71694384d3
jbenson6 <jbenson6@wisc.edu>
2017-04-13 16:47:36 -0500

    Update IntervalTree.java

--- p4/src/IntervalTree.java
+++ p4/src/IntervalTree.java
@@ -3,11 +3,13 @@ import java.util.List;
 public class IntervalTree<T extends Comparable<T>> implements IntervalTreeADT<T> {
 	
 	// TODO declare any data members needed for this class
-
+	private IntervalNode <T> root;
+	private int height;
+	private int size;
 	@Override
 	public IntervalNode<T> getRoot() {
 		// TODO Auto-generated method stub
-		return null;
+		return root;
 	}
 
 	@Override
@@ -28,39 +30,50 @@ public class IntervalTree<T extends Comparable<T>> implements IntervalTreeADT<T>
 	public IntervalNode<T> deleteHelper(IntervalNode<T> node,
 					IntervalADT<T> interval)
 					throws IntervalNotFoundException, IllegalArgumentException {
+						return node;
 		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public List<IntervalADT<T>> findOverlapping(
 					IntervalADT<T> interval) {
+						return null;
 		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public List<IntervalADT<T>> searchPoint(T point) {
+		
+		return null;
 		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public int getSize() {
+		return size;
 		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public int getHeight() {
+		return height;
 		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public boolean contains(IntervalADT<T> interval) {
+		return false;
 		// TODO Auto-generated method stub
 	}
 
 	@Override
 	public void printStats() {
 		// TODO Auto-generated method stub
-
+		System.out.println("-----------------------------------------");
+		System.out.println("Height: " + height);
+		System.out.println("Size: " + size);
+		System.out.println("-----------------------------------------");
 	}
 
 }
+
